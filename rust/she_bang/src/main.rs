6876f6718eadf204a8dfe4c656f0c9c23112d86c
sugiura-hiromichi <sugiura130418@icloud.com>
2023-02-26 10:44:13 +0900

    mod

--- rust/she_bang/src/main.rs
+++ rust/she_bang/src/main.rs
@@ -1,2 +1,14 @@
-fn main() {}
+use std::collections::HashMap;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error,>,> {
+	let resp =
+		reqwest::get("https://httpbin.org/ip",).await?.json::<HashMap<String, String,>>().await?;
+	println!("{:#?}", resp);
+
+	let client = reqwest::Client::new();
+	let _ = client.post("http://httpbin.org/post",).body("this is sent🫠🫠🫠🫠",).send().await?;
+	let rsp = reqwest::get("https://twitter.com/home",).await?.text().await?;
+	println!("|>	<|\n{rsp}");
+	Ok((),)
+}
